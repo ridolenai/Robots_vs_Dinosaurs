b6d52fb0956da829f2ef9afebaa02dc02f4f7d85
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-13 11:19:22 -0500

    Instantiated combatants
    main.py created and imports done.  robots and dinos established.

---
+++ battlefield.py
@@ -0,0 +1,6 @@
+
+
+class Battlefield:
+    def __init__(self):
+        pass
+    
---
+++ main.py
@@ -0,0 +1,17 @@
+from dinosaur import Dinosaur
+from fleet import Fleet
+from herd import Herd
+from robot import Robot
+from weapons import Weapons
+
+
+
+robot_one = Robot ('Jim_Bob', 100, 'stabby_stick')
+robot_two = Robot ('Bubba_Fred', 100, 'stabby_stick')
+robot_three = Robot ('James', 100, "stabby_stick")
+
+dino_one = Dinosaur ('Jaws', 100, 'bitey_mouth')
+dino_two = Dinosaur ('Teeth', 100, 'bitey_mouth')
+dino_three = Dinosaur('Roger', 100, 'bitey_mouth')
+
+
